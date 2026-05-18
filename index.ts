aed67167c6e6e659d39da9f713f157fa86493b5d
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-05-18 15:36:56 +0200

    feat:

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { localNetStaticConfig, SDK, type TokenProviderConfig } from '@canton-network/wallet-sdk';
+import { WalletSDKUtilitiesPlugin } from './src/extension';
 
 const auth: TokenProviderConfig = {
   method: 'self_signed',
@@ -11,13 +12,41 @@ const auth: TokenProviderConfig = {
   },
 };
 
-const sdk = SDK.create({
-  auth,
-  ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+const utilities = new WalletSDKUtilitiesPlugin();
+
+const sdk = (
+  await SDK.create({
+    auth,
+    ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+  })
+).registerPlugins([utilities]);
+
+const createParty = async (partyHint: string) => {
+  const keys = sdk.keys.generate();
+
+  const party = await sdk.party.external
+    .create(keys.publicKey, {
+      partyHint,
+    })
+    .sign(keys.privateKey)
+    .execute();
+
+  return { keys, party };
+};
+
+const receiver = await createParty('receiver');
+const operator = await createParty('operator');
+const instrumentAdmin = await createParty('instrumentAdmin');
+
+const preapprovalCommandExecutionResult = await utilities.preapprovalTransfer.create({
+  receiver: receiver.party.partyId,
+  operator: operator.party.partyId,
+  instrumentAdmin: instrumentAdmin.party.partyId,
+  instrumentAllowances: [
+    {
+      id: 'instr',
+    },
+  ],
 });
 
-const utilities = {
-  commands: {
-    transfer: {},
-  },
-};
+console.log(preapprovalCommandExecutionResult);
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,43 @@
-import type { SDKContext } from '@canton-network/wallet-sdk';
+import { SDKPlugin, type PreparedCommand } from '@canton-network/wallet-sdk';
 import { Utility } from './codegen/utility-registry-app-v0-0.7.0/lib';
+import type { Ops } from '@canton-network/core-provider-ledger';
 
-export class WalletSDKUtilitiesExtension {
-  constructor(private readonly ctx: SDKContext) {}
+export class WalletSDKUtilitiesPlugin extends SDKPlugin {
+  constructor() {
+    super('utilities');
+  }
 
-  public commands = {
-    createTransferPreapproval: (args: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval) => {
-      const { receiver, operator, instrumentAdmin, instrumentAllowances } = args;
-      return [
+  public preapprovalTransfer = {
+    create: async (args: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval) => {
+      const transferPreapprovalCommand: PreparedCommand<'CreateCommand'>[0] = {
+        CreateCommand: {
+          templateId: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval.templateId,
+          createArguments: args,
+        },
+      };
+
+      this.logger.info(
         {
-          CreateCommand: {
-            templateId: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval.templateId,
-            createArguments: {
-              operator,
-              receiver,
-              instrumentAdmin,
-              instrumentAllowances,
+          timestamp: new Date().toISOString(),
+          command: transferPreapprovalCommand,
+        },
+        'Successfully created transfer preapproval command. Executing...',
+      );
+
+      return await this.ctx.ledgerProvider.request<Ops.PostV2CommandsSubmitAndWaitForTransaction>({
+        method: 'ledgerApi',
+        params: {
+          resource: '/v2/commands/submit-and-wait-for-transaction',
+          requestMethod: 'post',
+          body: {
+            commands: {
+              commands: [transferPreapprovalCommand],
+              commandId: Bun.randomUUIDv7(),
+              actAs: [args.receiver],
             },
           },
         },
-      ];
+      });
     },
   };
 }

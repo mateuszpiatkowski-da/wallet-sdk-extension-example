0ada6033f725aca6dced5601c2289228b4c77353
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-05-21 09:07:31 +0200

    feat: update wallet-sdk version, move execution of command

--- index.test.ts
+++ index.test.ts
@@ -1,70 +1,88 @@
 import { localNetStaticConfig, SDK, type TokenProviderConfig } from '@canton-network/wallet-sdk';
 import { WalletSDKUtilitiesPlugin, WalletSDKUtilitiesPluginName } from './src/extension';
-import { expect, test, spyOn } from 'bun:test';
+import { expect, test } from 'bun:test';
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { $ } from 'bun';
 
-test('plugin connection', async () => {
-  const auth: TokenProviderConfig = {
-    method: 'self_signed',
-    issuer: 'unsafe-auth',
-    credentials: {
-      clientId: localNetStaticConfig.LOCALNET_USER_ID,
-      clientSecret: 'unsafe',
-      audience: 'https://canton.network.global',
-      scope: '',
-    },
-  };
-
-  const sdk = (
-    await SDK.create({
-      auth,
-      ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
-    })
-  ).registerPlugins({
-    [WalletSDKUtilitiesPluginName]: WalletSDKUtilitiesPlugin,
-  });
-
-  const createParty = async (partyHint: string) => {
-    const keys = sdk.keys.generate();
-
-    const party = await sdk.party.external
-      .create(keys.publicKey, {
-        partyHint,
-      })
-      .sign(keys.privateKey)
-      .execute();
-
-    return { keys, party };
-  };
-
-  const receiver = await createParty('receiver');
-  const operator = await createParty('operator');
-  const instrumentAdmin = await createParty('instrumentAdmin');
-
-  // Set up Bun spy on ledgerProvider.request
-  const plugin = sdk[WalletSDKUtilitiesPluginName];
-  const ledgerProvider = (plugin as any).ctx.ledgerProvider;
-  const ledgerRequestSpy = spyOn(ledgerProvider, 'request');
-
-  const preapprovalCommandExecutionResult = await sdk[WalletSDKUtilitiesPluginName].preapprovalTransfer.create({
-    receiver: receiver.party.partyId,
-    operator: operator.party.partyId,
-    instrumentAdmin: instrumentAdmin.party.partyId,
-    instrumentAllowances: [
-      {
-        id: 'instr',
+test(
+  'plugin connection',
+  async () => {
+    const auth: TokenProviderConfig = {
+      method: 'self_signed',
+      issuer: 'unsafe-auth',
+      credentials: {
+        clientId: localNetStaticConfig.LOCALNET_USER_ID,
+        clientSecret: 'unsafe',
+        audience: 'https://canton.network.global',
+        scope: '',
       },
-    ],
-  });
+    };
 
-  console.log(preapprovalCommandExecutionResult);
+    const sdk = (
+      await SDK.create({
+        auth,
+        ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+      })
+    ).registerPlugins({
+      [WalletSDKUtilitiesPluginName]: WalletSDKUtilitiesPlugin,
+    });
 
-  // Assert that ledgerProvider.request was called
-  expect(ledgerRequestSpy).toHaveBeenCalledTimes(1);
-  expect(ledgerRequestSpy).toHaveBeenCalledWith({
-    method: 'ledgerApi',
-    params: expect.objectContaining({
-      resource: '/v2/commands/submit-and-wait-for-transaction',
-      requestMethod: 'post',
-    }),
-  });
-});
+    const darFiles = await Promise.all(
+      (await $`bash ./scripts/latest-dars.sh`.text())
+        .split('\n')
+        .filter(Boolean)
+        .map(async (file) => ({
+          file: path.join(import.meta.dir, 'dar', file),
+          packageId: (await import(path.join(import.meta.dir, 'src', 'codegen', file.replace('.dar', '')))).packageId,
+        })),
+    );
+
+    for await (const { file, packageId } of darFiles) {
+      const bytes = new Uint8Array(await readFile(file));
+      await sdk.ledger.dar.upload(bytes, packageId);
+    }
+
+    const createParty = async (partyHint: string) => {
+      const keys = sdk.keys.generate();
+
+      const party = await sdk.party.external
+        .create(keys.publicKey, {
+          partyHint,
+        })
+        .sign(keys.privateKey)
+        .execute();
+
+      return { keys, party };
+    };
+
+    const receiver = await createParty('receiver');
+    const operator = await createParty('operator');
+    const instrumentAdmin = await createParty('instrumentAdmin');
+
+    const [preapprovalCommand, preapprovalDc] = sdk[WalletSDKUtilitiesPluginName].preapprovalTransfer.create({
+      receiver: receiver.party.partyId,
+      operator: operator.party.partyId,
+      instrumentAdmin: instrumentAdmin.party.partyId,
+      instrumentAllowances: [
+        {
+          id: 'instr',
+        },
+      ],
+    });
+
+    const result = await sdk.ledger
+      .prepare({
+        partyId: receiver.party.partyId,
+        commands: preapprovalCommand,
+        disclosedContracts: preapprovalDc,
+      })
+      .sign(receiver.keys.privateKey)
+      .execute({
+        partyId: receiver.party.partyId,
+      });
+
+    expect(result).toHaveProperty('updateId');
+  },
+  1000 * 60,
+);
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import { SDKPlugin, type PreparedCommand, type SDKContext } from '@canton-network/wallet-sdk';
 import { Utility } from './codegen/utility-registry-app-v0-0.7.0/lib';
-import type { Ops } from '@canton-network/core-provider-ledger';
 
 export const WalletSDKUtilitiesPluginName = 'utilities';
 
@@ -10,7 +9,9 @@ export class WalletSDKUtilitiesPlugin extends SDKPlugin {
   }
 
   public preapprovalTransfer = {
-    create: async (args: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval) => {
+    create: (
+      args: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval,
+    ): PreparedCommand<'CreateCommand'> => {
       const transferPreapprovalCommand: PreparedCommand<'CreateCommand'>[0] = {
         CreateCommand: {
           templateId: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval.templateId,
@@ -26,20 +27,7 @@ export class WalletSDKUtilitiesPlugin extends SDKPlugin {
         'Successfully created transfer preapproval command. Executing...',
       );
 
-      return await this.ctx.ledgerProvider.request<Ops.PostV2CommandsSubmitAndWaitForTransaction>({
-        method: 'ledgerApi',
-        params: {
-          resource: '/v2/commands/submit-and-wait-for-transaction',
-          requestMethod: 'post',
-          body: {
-            commands: {
-              commands: [transferPreapprovalCommand],
-              commandId: Bun.randomUUIDv7(),
-              actAs: [args.receiver],
-            },
-          },
-        },
-      });
+      return [transferPreapprovalCommand, []];
     },
   };
 }

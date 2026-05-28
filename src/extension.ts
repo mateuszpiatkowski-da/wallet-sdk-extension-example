147301c37486d3696e492fe5d2505404fa9aeb4a
Mateusz Piątkowski <mateuszpiatkowski@digitalasset.com>
2026-05-28 09:43:45 +0200

    adjust import style

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,21 @@
 import { SDKPlugin, type PreparedCommand, type SDKContext } from '@canton-network/wallet-sdk';
-import { Utility } from './codegen/utility-registry-app-v0-0.7.0/lib';
+import * as UtilityRegistryApp from './codegen/utility-registry-app-v0-0.7.0/lib';
 
 export const WalletSDKUtilitiesPluginName = 'utilities';
 
 export class WalletSDKUtilitiesPlugin extends SDKPlugin {
-  constructor(protected override readonly ctx: SDKContext) {
+  constructor(ctx: SDKContext) {
     super(WalletSDKUtilitiesPluginName, ctx);
   }
 
   public preapprovalTransfer = {
     create: (
-      args: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval,
+      args: UtilityRegistryApp.Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval,
     ): PreparedCommand<'CreateCommand'> => {
       const transferPreapprovalCommand: PreparedCommand<'CreateCommand'>[0] = {
         CreateCommand: {
-          templateId: Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval.templateId,
+          templateId:
+            UtilityRegistryApp.Utility.Registry.App.V0.Model.TransferPreapproval.TransferPreapproval.templateId,
           createArguments: args,
         },
       };
